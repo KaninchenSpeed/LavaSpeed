907e89996d2677a2691604cf30a17d5c7d3c793d
kaninchenspeed <info@it-otterstadt.de>
2023-12-18 22:39:45 +0100

    fix createive and spectator flight

--- src/main/java/de/itotterstadt/lavaspeed/listeners/MoveListener.kt
+++ src/main/java/de/itotterstadt/lavaspeed/listeners/MoveListener.kt
@@ -17,9 +17,14 @@ class MoveListener: Listener {
     fun onMove(event: PlayerMoveEvent) {
         if (event.player.location.add(Vector(0f, 0.75f, 0f)).block.type != Material.LAVA) {
             event.player.walkSpeed = 0.2f
-            event.player.isFlying = false
+            if (event.player.gameMode != GameMode.SPECTATOR) {
+                event.player.flySpeed = 0.1f
+            }
             if (event.player.gameMode == GameMode.SURVIVAL || event.player.gameMode == GameMode.ADVENTURE) {
                 event.player.allowFlight = false
+                event.player.isFlying = false
+            } else {
+                event.player.allowFlight = true
             }
             return
         }
