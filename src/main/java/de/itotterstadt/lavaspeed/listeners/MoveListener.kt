195093cff019349b977db4e10b86e83216b7ce45
kaninchenspeed <info@it-otterstadt.de>
2023-12-20 19:04:04 +0100

    only apply/remove speed when entering/leaving lava

--- src/main/java/de/itotterstadt/lavaspeed/listeners/MoveListener.kt
+++ src/main/java/de/itotterstadt/lavaspeed/listeners/MoveListener.kt
@@ -1,9 +1,11 @@
 package de.itotterstadt.lavaspeed.listeners
 
+import com.destroystokyo.paper.event.player.PlayerConnectionCloseEvent
 import de.itotterstadt.lavaspeed.configuration
 import de.itotterstadt.lavaspeed.plugin
 import org.bukkit.GameMode
 import org.bukkit.Material
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerMoveEvent
@@ -14,6 +16,8 @@ class MoveListener: Listener {
     private val teams = configuration?.getStringList("teams")
     private val lavaSpeed = configuration?.getDouble("lavaSpeed")?.toFloat() ?: 0.3f
     private val lavaSpeedEffect = configuration?.getDouble("lavaSpeedEffect")?.toFloat() ?: 0.05f
+
+    private val inLava = HashSet<String>()
 
     init {
         if (configuration!!.get("lavaSpeed") == null) {
@@ -28,20 +32,31 @@ class MoveListener: Listener {
 
     @EventHandler
     fun onMove(event: PlayerMoveEvent) {
-        if (event.player.location.add(Vector(0f, 0.75f, 0f)).block.type != Material.LAVA) {
-            event.player.walkSpeed = 0.2f
-            if (event.player.gameMode != GameMode.SPECTATOR) {
-                event.player.flySpeed = 0.1f
+        val isInLava = event.player.location.add(Vector(0f, 0.75f, 0f)).block.type == Material.LAVA
+        val wasInLava = inLava.contains(event.player.uniqueId.toString())
+
+        if (wasInLava) {
+            if (!isInLava) {
+                inLava.remove(event.player.uniqueId.toString())
+
+                event.player.walkSpeed = 0.2f
+                if (event.player.gameMode != GameMode.SPECTATOR) {
+                    event.player.flySpeed = 0.1f
+                }
+                if (event.player.gameMode == GameMode.SURVIVAL || event.player.gameMode == GameMode.ADVENTURE) {
+                    event.player.allowFlight = false
+                    event.player.isFlying = false
+                } else {
+                    event.player.allowFlight = true
+                }
             }
-            if (event.player.gameMode == GameMode.SURVIVAL || event.player.gameMode == GameMode.ADVENTURE) {
-                event.player.allowFlight = false
-                event.player.isFlying = false
-            } else {
-                event.player.allowFlight = true
-            }
+
+            return
+        } else if (!isInLava) {
             return
         }
 
+        inLava.add(event.player.uniqueId.toString())
 
         val team = plugin!!.server.scoreboardManager.mainScoreboard.teams.find {
             it.hasPlayer(event.player)
@@ -54,6 +69,13 @@ class MoveListener: Listener {
         event.player.isFlying = true
         event.player.walkSpeed = lavaSpeedEffect
         event.player.flySpeed = lavaSpeed
+    }
+
+    @EventHandler
+    fun onLeave(event: PlayerConnectionCloseEvent) {
+        if (inLava.contains(event.playerUniqueId.toString())) {
+            inLava.remove(event.playerUniqueId.toString())
+        }
     }
 
 }
